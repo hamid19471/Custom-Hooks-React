aa2603a0cf120f394309e7e011f6cd0b01afd62c
Hamid Asadi <hamid.19471@gmail.com>
2022-11-05 00:15:00 +0330

    import created custom hook and component here

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import CounterActions from "./Context/CounterActions";
+import CounterProvider from "./Context/CounterProvider";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <CounterProvider>
+                <CounterActions />
+            </CounterProvider>
+        </div>
+    );
 }
 
 export default App;
